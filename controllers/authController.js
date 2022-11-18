e2c47d34abd5837029ee873512a7c3bedb09505c
phamlong9102 <phamlong9102@gmail.com>
2022-11-18 13:31:47 +0700

    Api: Login successfully

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,10 +14,29 @@ const authController = {
                 password: hashedPassword,
             });
             // Save new user to Database
-            const user = newUser.save();
-            res.status(200).json(user);
+            await newUser.save();
+            res.status(200).json("Register successfully");
         } catch (err) {
             res.status(500).json(err);
+        }
+    },
+
+    // Login
+    loginUser: async (req, res) => {
+        try {
+            const user = await User.findOne({userName: req.body.userName});
+            if(!user) {
+                res.status(404).json
+            }
+            const validPassword = await bcrypt.compare(req.body.password, user.password)
+            if(!validPassword) {
+                res.status(404).json("Wrong password")
+            }
+            if(user && validPassword) {
+                res.status(200).json(user);
+            }
+        } catch (err) {
+            res.status(500).json();
         }
     },
 };
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,12 @@ const authController = require("../controllers/authController");
 
 const router = require("express").Router(); 
 
+// Regiter route
 router.post("/register", authController.userRegister)
+
+// Login route
+router.post("/login", authController.loginUser)
+
 
 module.exports = router;    
 
